e4b1096b3febe737ccce9bee5b4219c06c53f031
root <root@3c50fc669a74>
2020-07-09 21:11:47 +0000

    finished

---
+++ src/_05_for_loops/_2_badgers/Badger.java
@@ -0,0 +1,30 @@
+package _05_for_loops._2_badgers;
+
+public class Badger {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int b = 0; b <100; b++) {
+			
+		
+		for (int i = 0; i < 12; i++) {
+			System.out.println("Badger,");
+			
+		}
+		for (int j = 0; j < 2; j++) {
+			System.out.println("Mushroom,");
+				
+			}
+		for (int i = 0; i <1; i++) {
+			System.out.println("A snake!");
+			
+		}
+		
+		
+		
+		
+	}
+}
+}
---
+++ src/_05_for_loops/_4_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,29 @@
+void setup(){
+  size(600,600);
+  
+  background(#C60AC6);
+  for (int i = 0; i < 3; i++){
+    text("ice cream",100,i*10+10);
+    
+    
+    
+  }
+  text("banana",100,40);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
